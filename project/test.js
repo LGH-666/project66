8795aaa8ec1b4c08a92a2bf1f70042daafe4a650
lgh <lgh145978@163.com>
2021-05-07 16:33:19 +0800

    master的第一次提交

---
+++ project/test.js
@@ -0,0 +1,18 @@
+function fn3() {
+  let a = 2;
+  let b = 7
+  console.log(a * b);
+}
+fn3()
+
+function fn1(b, c, d) {
+  console.log(b, c, d);
+}
+fn1()
+
+function fn2() {
+  let a = 3
+  let b = 4
+  console.log(a * b * b);
+}
+fn2()
---
+++ project1/test.js
@@ -0,0 +1,14 @@
+console.log('1');
+
+function fn() {
+  let a = 1
+  let b = 4
+  console.log(a + b);
+}
+console.log(2);
+function fn1() {
+  console.log('媳妇儿，真美');
+}
+function fn2() {
+  console.log('今天天气真美');
+}
